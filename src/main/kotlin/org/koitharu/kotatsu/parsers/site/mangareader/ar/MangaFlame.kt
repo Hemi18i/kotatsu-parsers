9a35a7c4f0f311adcba101da24a7fb7f510a8ff8
Hemi18i <pubgroblox906@gmail.com>
2025-07-27 09:12:27 +0300

    Update MangaFlame.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/MangaFlame.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/MangaFlame.kt
@@ -5,6 +5,6 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@MangaSourceParser("MANGAFLAME", "MangaFlame", "ar")
+@MangaSourceParser("MANGAFLAME", "MangaFlame", "en")
 internal class MangaFlame(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.MANGAFLAME, "mangaflame.org", pageSize = 20, searchPageSize = 10)
