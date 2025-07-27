f22445303bda701d7d77a0ea3f0ca45618d7233b
Hemi18i <pubgroblox906@gmail.com>
2025-07-27 08:13:39 +0300

    Update MangaTime.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/MangaTime.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/MangaTime.kt
@@ -10,11 +10,4 @@ internal class MangaTime(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.MANGATIME, "mangatime.org") {
 	
 	override val datePattern = "d MMMM، yyyy"
-	override val isTagsExclusionSupported = false
-	override val listUrl = "manga/"
-	
-	// تخصيصات للمحتوى العربي والكوري المختلط
-	override val selectMangaListImg = "img"
-	override val selectChapterDate = "span.chapter-release-date i"
-	override val selectChapterUrl = "li.wp-manga-chapter > a"
 }
