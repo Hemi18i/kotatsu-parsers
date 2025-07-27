9b1c16ab5268028a34580e060f6775da07339925
Hemi18i <pubgroblox906@gmail.com>
2025-07-27 09:47:02 +0300

    Update RocksManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/RocksManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/RocksManga.kt
@@ -12,4 +12,20 @@ internal class RocksManga(context: MangaLoaderContext) :
 	override val datePattern = "d MMMM yyyy"
 	override val listUrl = "manga/"
 	override val tagPrefix = "manga-genre/"
+	
+	// إضافة timeouts أطول للتعامل مع بطء الموقع
+	override val configKeyDomain = "rockscans.org"
+	
+	// إضافة headers مخصصة إذا كان الموقع يتطلبها
+	override fun onCreateConfig(keys: MutableCollection<String>) {
+		super.onCreateConfig(keys)
+		keys.add(configKeyDomain)
+	}
+	
+	// التعامل مع مشاكل الترميز المحتملة للنصوص العربية
+	override val selectChapter = "li.wp-manga-chapter > a"
+	override val selectPage = "div.reading-content img"
+	
+	// إضافة User-Agent مخصص إذا كان الموقع يحجب البوتات
+	override val userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
